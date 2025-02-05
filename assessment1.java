16f7ddb8b3141e3394a61f18a77d61641811da4a
tip-aaron <qaamragudos@tip.edu.ph>
2025-02-05 12:08:03 +0800

    Update assessment1.java

--- assessment1.java
+++ assessment1.java
@@ -4,46 +4,46 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-	    println("Name: " + State.NAME);
-	    println("Section: " + State.SECTION);
-	    
-	    State state = new State();
-	    Scanner scanner = new Scanner(System.in);
-	    
-	    print("Enter student name: ");
-	    state.setStudentName(scanner.nextLine());
-	    
-	    print("Enter course code 1: ");
-	    state.setCourseCode1(scanner.nextLine());
-	    
-	    print("Enter course grade 1: ");
-	    state.setCourseGrade1(Double.parseDouble(scanner.nextLine()));
-	    
-	    print("Enter course unit 1: ");
-	    state.setCourseUnit1(Integer.parseInt(scanner.nextLine()));
-	    
-	    print("Enter course code 2: ");
-	    state.setCourseCode2(scanner.nextLine());
-	    
-	    print("Enter course grade 2: ");
-	    state.setCourseGrade2(Double.parseDouble(scanner.nextLine()));
-	    
-	    print("Enter course unit 2: ");
-	    state.setCourseUnit2(Integer.parseInt(scanner.nextLine()));
-	    
-	    scanner.close();
-	    
-	    println("Your General Weighted Average (GWA): " + Math.round(state.getGWA()));
+		    println("Name: " + State.NAME);
+		    println("Section: " + State.SECTION);
+		    
+		    State state = new State();
+		    Scanner scanner = new Scanner(System.in);
+		    
+		    print("Enter student name: ");
+		    state.setStudentName(scanner.nextLine());
+		    
+		    print("Enter course code 1: ");
+		    state.setCourseCode1(scanner.nextLine());
+		    
+		    print("Enter course grade 1: ");
+		    state.setCourseGrade1(Double.parseDouble(scanner.nextLine()));
+		    
+		    print("Enter course unit 1: ");
+		    state.setCourseUnit1(Integer.parseInt(scanner.nextLine()));
+		    
+		    print("Enter course code 2: ");
+		    state.setCourseCode2(scanner.nextLine());
+		    
+		    print("Enter course grade 2: ");
+		    state.setCourseGrade2(Double.parseDouble(scanner.nextLine()));
+		    
+		    print("Enter course unit 2: ");
+		    state.setCourseUnit2(Integer.parseInt(scanner.nextLine()));
+		    
+		    scanner.close();
+		    
+		    println("Your General Weighted Average (GWA): " + Math.round(state.getGWA()));
 	}
 	
 	private static void print(String msg)
 	{
-	    System.out.print(msg);
+		    System.out.print(msg);
 	}
 	
 	private static void println(String msg)
 	{
-	    System.out.println(msg);
+		    System.out.println(msg);
 	}
 }
 
@@ -58,50 +58,50 @@ class State
     
     public void setStudentName(String name)
 	{
-        studentName = name;
+	        studentName = name;
 	}
     
     public void setCourseCode1(String cc1)
 	{
-        courseCode1 = cc1;
+	        courseCode1 = cc1;
 	}
     
     public void setCourseCode2(String cc2)
 	{
-        courseCode2 = cc2;
+	        courseCode2 = cc2;
 	}
     
     public void setCourseUnit1(int cu1) 
 	{
-        courseUnit1 = cu1;
-        
-        calculateTotalUnits();
+	        courseUnit1 = cu1;
+	        
+	        calculateTotalUnits();
 	}
     
     public void setCourseUnit2(int cu2) 
 	{
-        courseUnit2 = cu2;
-        
-        calculateTotalUnits();
+	        courseUnit2 = cu2;
+	        
+	        calculateTotalUnits();
 	}
     
     public void setCourseGrade1(double cg1)
 	{
-        courseGrade1 = cg1;
-    }
+        	courseGrade1 = cg1;
+	}
     
     public void setCourseGrade2(double cg2)
 	{
-        courseGrade2 = cg2;
+        	courseGrade2 = cg2;
 	}
     
     public double getGWA()
 	{
-        return (courseGrade1 * courseUnit1 + courseGrade2 * courseUnit2) / totalUnits;
+      		return (courseGrade1 * courseUnit1 + courseGrade2 * courseUnit2) / totalUnits;
 	}
     
     private void calculateTotalUnits()
 	{
-        totalUnits = courseUnit1 + courseUnit2;
+       		totalUnits = courseUnit1 + courseUnit2;
 	}
 }
